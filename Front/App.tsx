8f039d59ff52e33c973cb45b1e44cd5d2ebb4b8e
Sanchez988 <valentina.roldan339@pascualbravo.edu.co>
2026-05-12 19:13:55 -0500

    Update App.tsx

--- Front/App.tsx
+++ Front/App.tsx
@@ -5,11 +5,13 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/context/AuthContext';
 import { ThemeProvider, useTheme } from './src/context/ThemeContext';
 import Navigation from './src/navigation';
+import { EasUpdatePrompt } from './src/components/EasUpdatePrompt';
 
 const AppContent = () => {
   const { isDark } = useTheme();
   return (
     <>
+      <EasUpdatePrompt />
       <Navigation />
       <StatusBar style={isDark ? 'light' : 'dark'} />
     </>
